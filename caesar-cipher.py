c556f08abf96b95b3af42f6f30360cc5135ef4df
oliver <oshmusic11@gmail.com>
2025-04-05 17:13:58 -0400

    Updated comments and error handling

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -5,23 +5,7 @@ from tkinter import *
 from tkinter import ttk
 import re
 
-# Hardcoded alphabet reference (no longer used but included for now)
-# charToInt = {
-#     'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5,
-#     'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10,
-#     'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15,
-#     'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20,
-#     'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26
-# }
-
-# intToChar = {
-#     1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E',
-#     6: 'F', 7: 'G', 8: 'H', 9: 'I', 10: 'J',
-#     11: 'K', 12: 'L', 13: 'M', 14: 'N', 15: 'O',
-#     16: 'P', 17: 'Q', 18: 'R', 19: 'S', 20: 'T',
-#     21: 'U', 22: 'V', 23: 'W', 24: 'X', 25: 'Y', 26: 'Z'
-# }
-
+# Variables
 num_ranges = ['0', '25', '24', '23', '22', '21', '20', '19', '18', '17', '16', '15', 
               '14', '13', '12', '10', '9', '8', '7', '6', '5', '4', '3', '2', '1', 
               '-1', '-2', '-3', '-4', '-5', '-6', '-7', '-8', '-9', '-10',
@@ -31,7 +15,6 @@ num_ranges = ['0', '25', '24', '23', '22', '21', '20', '19', '18', '17', '16', '
 # Logic
 
 # Helper functions
-
 def int_to_char(val: int) -> str:
     """Given an ASCII value as an integer, returns the character it represents"""
     return chr(val)
@@ -90,24 +73,34 @@ def shift_rec(text: str, val: int) -> str:
 
 # Shift function, performs data cleaning
 def shift(text: str, val: int) -> str:
+    """Takes in a string of text, checks that it contains only ASCII characters,
+        cleans the string, calls a shift function on the
+        string, and returns the result"""
     if (not text.isascii()):
         return "Error: non-ASCII characters present"
     text = ascii(text).rstrip('\'').lstrip('\'')
     return shift_it(text, val)
 
-
+# Main computation function; interacts with GUI elements to call shift()
 def compute(*args):
+    """Retrieves values from the GUI elements to perform the shift computation"""
     # clear result window
     txt_result.delete('1.0', 'end')
     try:
-        shift_int = int(shiftval.get())
+        try:
+            shift_int = int(shiftval.get())
+        except ValueError:
+            txt_result.insert('1.0', 'Please select a shift value')
+            pass
         txt_result.insert('1.0', shift(txt_entry.get('1.0', 'end').rstrip('\n'), shift_int))
     except ValueError:
-        txt_result.insert('1.0', 'Please select a shift value')
+        txt_result.insert('1.0', 'An error occurred during computation!')
         pass
 
 
 # GUI
+
+# This section of code (and the compute() function above) contains elements of code from TkDocs' basic tutorial
 
 # Main application window
 root = Tk()
