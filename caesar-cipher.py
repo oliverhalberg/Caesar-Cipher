f6a60f691ae0e2d2ff5c87281563162be00fe1b2
oliver <oshmusic11@gmail.com>
2025-04-07 16:17:06 -0400

    Minor format edits to comments on caesar-cipher.py

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -8,9 +8,9 @@ import re
 # Global variables
 num_ranges = ['25', '24', '23', '22', '21', '20', '19', '18', '17', '16', '15', 
               '14', '13', '12', '10', '9', '8', '7', '6', '5', '4', '3', '2', '1', 
-              '-1', '-2', '-3', '-4', '-5', '-6', '-7', '-8', '-9', '-10',
-              '-11', '-12', '-13', '-14', '-15', '-16', '-17', '-18', '-19', '-20',
-              '-21', '-22', '-23', '-24', '-25']
+              '-1', '-2', '-3', '-4', '-5', '-6', '-7', '-8', '-9', '-10', '-11', 
+              '-12', '-13', '-14', '-15', '-16', '-17', '-18', '-19', '-20', '-21',
+              '-22', '-23', '-24', '-25']
 
 # Logic
 
@@ -101,7 +101,8 @@ def compute(*args):
 
 # GUI
 
-# This section of code (and the compute() function above) contains elements of code from TkDocs' basic tutorial
+# This section of code (and the compute() function above) contains elements of code from 
+# TkDocs' basic tutorial (see README.md for link)
 
 # Main application window
 root = Tk()
